c9c222ddf0cac55d04ca80d9d16dd18f53103826
--get <--get>
2025-07-14 10:03:01 +0100

    style: add formating to files

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-
 //select items
 
 const btn = document.querySelector(".btn");
-const closeBtn = document.querySelector("#closebtn")
-const popup = document.querySelector("#popup")
+const closeBtn = document.querySelector("#closebtn");
+const popup = document.querySelector("#popup");
 
+btn.addEventListener("click", function () {
+  popup.classList.add("open-popup");
+});
 
-btn.addEventListener('click', function(){
-    popup.classList.add("open-popup")
-})
-
-closeBtn.addEventListener("click", function(){
-    popup.classList.remove("open-popup")
-})
+closeBtn.addEventListener("click", function () {
+  popup.classList.remove("open-popup");
+});
